b325a9a5fd5c794e12ee93a7225faa793596afc6
Ju Hong Kim <zakuarbor@proton.me>
2026-02-28 23:07:41 -0500

    Deploy: 2026-02-28 23:07:41

--- _site/deploy.sh
+++ _site/deploy.sh
@@ -8,7 +8,6 @@ NC='\033[0m'
 echo -e "${BLUE}Starting Jekyll Build...${NC}"
 
 jekyll build --destination _site
-cp .domains _site
 
 if [ $? -ne 0 ]; then
     echo -e "${RED}❌ Jekyll build failed. Check your Gemfile.${NC}"
@@ -16,6 +15,11 @@ if [ $? -ne 0 ]; then
 fi
 
 cd _site || exit 1
+
+echo -e "${BLUE}Creating .domains ...${NC}"
+
+echo "randombits.ca" > .domains
+echo "www.randombits.ca" > .domains
 
 if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
     echo -e "${RED}❌ _site is not linked to Git. Run the reset commands above.${NC}"
@@ -32,5 +36,5 @@ cd ..
 
 echo -e "${GREEN}✅ GitHub Pages updated!${NC}"
 
-neocities push _site
+#neocities push _site
 
